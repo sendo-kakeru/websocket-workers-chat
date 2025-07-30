d5a939b06d52a08d4f65cba40dd40642e26e842f
sendo-kakeru <sendokakeru.js@gmail.com>
2025-07-30 18:41:02 +0900

    feat: tanstack router

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,23 @@
-import { reactRouter } from "@react-router/dev/vite";
+import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
